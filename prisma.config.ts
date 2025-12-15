9c7c3ce241691db03a18fd126dfcfda27765d9e0
Enzo <enzosousa1@yahoo.com>
2025-12-15 21:05:10 +0100

    fix/ add default url in prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -9,6 +9,8 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: env("DATABASE_URL"),
+    url:
+      env("DATABASE_URL") ||
+      "postgresql://postgres:postgres@localhost:5432/postgres",
   },
 });
