f61e9dcd0dd3bfeffcfcef6906179be6b34528e8
Enzo <enzosousa1@yahoo.com>
2026-01-05 20:35:45 +0100

    fix/  create reset password component for use searchparams into client side

---
+++ app/(public)/reset-password/reset-password-form.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { authClient } from "@/lib/auth-client";
+import { useRouter, useSearchParams } from "next/navigation";
+import { toast } from "sonner";
+
+export function ResetPasswordForm() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const token = searchParams.get("token");
+
+  if (!token) {
+    toast.error("Token invalide");
+    return null;
+  }
+
+  async function onSubmit(formData: FormData) {
+    const password = formData.get("password");
+    await authClient.resetPassword(
+      {
+        newPassword: String(password),
+        token: String(token),
+      },
+      {
+        onSuccess: () => {
+          router.push(`/signin`);
+          toast.success("Mot de passe réinitialisé, veuillez vous connecter");
+        },
+        onError: (error) => {
+          toast.error(error.error.message);
+        },
+      },
+    );
+  }
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Réinitialiser le mot de passe</CardTitle>
+        <CardDescription className="text-sm text-muted-foreground mt-1">
+          Entrez votre nouveau mot de passe ci-dessous.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form className="flex flex-col gap-4" action={onSubmit}>
+          <div className="space-y-2">
+            <Label htmlFor="password">Nouveau mot de passe</Label>
+            <Input type="password" id="password" name="password" />
+          </div>
+          <Button type="submit">Réinitialiser le mot de passe</Button>
+        </form>
+      </CardContent>
+    </Card>
+  );
+}
